02fbab17217474b7d0674707f8dc2c9d5aef0376
Toshi <pumpkinpie.tea@gmail.com>
2019-02-17 01:29:03 +0900

    initial commit

---
+++ kadai1/kadai1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const inputnumber = parseInt(window.prompt('数字を入力してください。'))
+for (let i = 1; i <= inputnumber; i++) {
+  if (i % 15 === 0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
+  }
+}
