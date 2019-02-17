8ff2a8814cfdba87de8e94bef6e07cd6e34ed300
Toshio Imaoka <pumpkinpie.tea@gmail.com>
2019-02-17 22:46:06 +0900

    finish kadai3

--- kadai3/kadai3.js
+++ kadai3/kadai3.js
@@ -1,17 +1,40 @@
 'use strict'
 
-const computerNumber = Math.floor(Math.random() * 3)
-let inputNumber = parseInt(window.prompt('「じゃんけん・・・」\n 0.グー 1.チョキ 2.パー'))
-let status = ''
-while (computerNumber !== inputNumber) {
-  if (computerNumber == > inputNumber) {
-    status = 'もっと上'
+function janken() {
+  const computer = Math.floor(Math.random() * 3)
+  const sentence = '「じゃんけん・・・」\n 0.グー 1.チョキ 2.パー'
+  const user = parseInt(window.prompt(sentence))
+  console.log(sentence)
+  console.log(user + '\n「ぽい！」\n＊コンピュータ：' + jankenText(computer) + '\n＊あなた　：' + jankenText(user))
+  if (computer === user) {
+    console.log('「あいこでしょ！」')
+    janken()
+  } else if ((computer === 0 && user === 2) || (computer === 1 && user === 0) || (computer === 2 && user === 1)) {
+    console.log('「あなたの勝ち！」')
+  } else if ((computer === 0 && user === 1) || (computer === 1 && user === 2) || (computer === 2 && user === 0)) {
+    console.log('コンピュータの勝ち！')
   } else {
-    status = 'もっと下'
+    console.log('error')
   }
-  console.log(inputNumber)
-  console.log(status)
-  inputNumber = parseInt(window.prompt(inputNumber + '\n' + status))
 }
-console.log(inputNumber)
-console.log('正解！・・・' + counter + '回目で当てました')
+
+function jankenText(n) {
+  let outputText = ''
+  switch (n) {
+    case 0:
+      outputText = 'グー'
+      break
+    case 1:
+      outputText = 'チョキ'
+      break
+    case 2:
+      outputText = 'パー'
+      break
+    default:
+      outputText = 'error'
+      break
+  }
+  return outputText
+}
+
+janken()
